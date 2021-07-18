98c73646d7229bcbdb0d0167e7f861f87d2f1418
Roddied <valenmalino@gmail.com>
2021-07-18 22:40:59 +0300

    18.07_22.40

--- Program.cs
+++ Program.cs
@@ -20,24 +20,30 @@ namespace Qwest
             return d10;
         }
     }
-    class Greetings { 
-    
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Little Mouse\nWhat is your name?");
+            Console.WriteLine("After stepping through the basement door you see smiled  old mouse with red fur and grey cape.\nHe was reading some letter, but when you came - he hid it and looked at you.");
+            Console.ReadKey(true);
+            Console.WriteLine("Hello Little Mouse! My name's Redmand\nAnd what is yours?");
             string Name = Console.ReadLine();
-            await.Task.Delay(50);
-            Console.WriteLine("Oh, hello, " + Name + ". And I'm Kaleb - your mentor for this test.");
+            Console.WriteLine("So, " + Name + ", in this dim light I can't see what color of your fur. What is it?");
+            string Fur = Console.ReadLine();
+            Console.WriteLine("Oh, " + Fur + " fur. Yeah, I knew few folks with that color.");
+            switch (Fur) {
+                case "Red" or "red":
+                    Console.WriteLine("There was one redfur that had outstanding might. Once he become Guard he slayed snake and owl in two seasons.");
+                    break;
+                case "Grey" or "grey":
+                    Console.WriteLine("One bluefur folk was the master of mind and staff.");
+                    break;
+
+            }
             int d20 = Dice.D20();
             int d8 = Dice.D8();
             int d10 = Dice.D10();
             Console.WriteLine(d20 + ", " + d8 + ", " + d10);
-            
-            
-            
         }
     }
 }
